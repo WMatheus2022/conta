e5749f8e44812179970248e225ca1798f733e45b
WMatheus2022 <pachwander4@gmail.com>
2024-05-02 09:46:11 -0300

    projeto banco java

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+public class ContaCorrente {
+    
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){}
+    public void depositar(){}
+    public int getAgencia() {
+        return agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void transferir(){}
+}
