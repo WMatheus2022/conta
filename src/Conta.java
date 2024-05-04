d104379e168118be7992a8e5fed3adf6929be80b
WMatheus2022 <pachwander4@gmail.com>
2024-05-04 11:58:13 -0300

    Pilares da orientacao a obj - Polimorfismo

--- src/Conta.java
+++ src/Conta.java
@@ -1,24 +1,31 @@
-public abstract class Conta implements IConta{
+public abstract class Conta implements IConta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 1;
 
-    
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void depositar(double valor) {
-        
+
     }
 
     @Override
     public void sacar(double valor) {
-        
+
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
-        
+
     }
 
     public int getAgencia() {
@@ -32,6 +39,5 @@ public abstract class Conta implements IConta{
     public double getSaldo() {
         return saldo;
     }
-
 
 }
